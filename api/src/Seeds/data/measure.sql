9a3e2332cb82dd0ba27f544566e188e8c2b217d2
tesav <tesav@yandex.ru>
2022-07-18 00:19:21 +0300

    Development (#2)
    * init
    
    * init
    
    * added db data
    
    * delete unused
    
    * delete unused
    
    * fixed entities
    
    * fixed entities
    
    * fixed entities
    
    * fixed entities
    
    * fixed entities
    
    * fixed entities
    
    * fixed entities
    
    * fixed entities
    
    * fixed entities
    
    Co-authored-by: Alex <tesav1978@gmail.com>

---
+++ api/src/Seeds/data/measure.sql
@@ -0,0 +1,5 @@
+INSERT INTO shop.measure (id, name) VALUES (0, 'шт');
+INSERT INTO shop.measure (id, name) VALUES (1, 'кг');
+INSERT INTO shop.measure (id, name) VALUES (2, 'уп');
+INSERT INTO shop.measure (id, name) VALUES (3, 'л');
+INSERT INTO shop.measure (id, name) VALUES (6, '100г');
